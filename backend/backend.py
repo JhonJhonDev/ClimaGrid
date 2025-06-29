3f9cab6c12f276903c45b40d5dd772ac0cfd8f8f
FrogSpeedG <neilmehra2008@gmail.com>
2025-06-29 00:42:11 -0400

    added temp integration

--- backend/backend.py
+++ backend/backend.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from heat import simheat
 from energy import calculate_energy_usage
+import xarray as xr
+import numpy as np
+import pandas as pd
+
 app = Flask(__name__)
 CORS(app)
 
@@ -21,7 +25,13 @@ def evaluate():
             for j in range(grid_size):
                 temp.append(grid[i][j]["type"])
             flat.append(temp)
-        flatsim = simheat(flat,airtemp = 20)
+        url = "https://ds.nccs.nasa.gov/thredds/dodsC/AMES/NEX/GDDP-CMIP6/ACCESS-CM2/ssp245/r1i1p1f1/tasmax/tasmax_day_ACCESS-CM2_ssp245_r1i1p1f1_gn_" + str(year) +".nc"
+        dstm = xr.open_dataset(url)
+        tasmax_c = dstm['tasmax']-273.15
+        value = tasmax_c.sel(lat=latitude, lon=longitude, method='nearest')
+        airtemp = value.max(dim="time")
+        
+        flatsim = simheat(flat,airtemp)
         
         # Calculate energy usage heatmap and statistics
         energy_heatmap, energy_stats = calculate_energy_usage(flat)
