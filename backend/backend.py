3ba3c8d55f4c8402255efae75d03007f4cecf058
JhonJhonDev <jhonissmart@gmail.com>
2025-06-29 01:31:08 -0400

    Fixed backend

--- backend/backend.py
+++ backend/backend.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from heat import simheat
 from energy import calculate_energy_usage
+import random
+import math
 import xarray as xr
 import numpy as np
 import pandas as pd
@@ -16,8 +18,8 @@ def evaluate():
         print("Received evaluation data:")
         grid_size = data.get('gridSize')
         grid = data.get('grid')
-        longitude = int(data.get('longitude'))
-        latitude = int(data.get('latitude'))
+        longitude = float(data.get('longitude')) + 180
+        latitude = float(data.get('latitude')) 
         year = str(data.get('yr'))
         evaluation = data.get('evaluation')
         flat = []
@@ -29,10 +31,28 @@ def evaluate():
         url = "https://ds.nccs.nasa.gov/thredds/dodsC/AMES/NEX/GDDP-CMIP6/ACCESS-CM2/ssp245/r1i1p1f1/tasmax/tasmax_day_ACCESS-CM2_ssp245_r1i1p1f1_gn_" + str(year) +".nc"
         dstm = xr.open_dataset(url,engine='netcdf4' )
         tasmax_c = dstm['tasmax']-273.15
-        value = tasmax_c.sel(lat=latitude, lon=longitude, method='nearest')
-        airtemp = value.max(dim="time")
-        airtemp = airtemp.item()
+        latmin = dstm.lat.min().item()
+        latmax = dstm.lat.max().item()
+        lonmin = dstm.lon.min().item()
+        lonmax = dstm.lon.max().item()
+        print("Lat range:", dstm.lat.min().item(), dstm.lat.max().item())
+        print("Lon range:", dstm.lon.min().item(), dstm.lon.max().item())
+        #clamp functions because data is fucked
+        if longitude > lonmax:
+            longitude = lonmax
+        elif longitude < lonmin:
+            longitude = lonmin
         
+        if latitude > latmax:
+            latitude = latmax
+        elif longitude < lonmin:
+            latitude = latmin
+        value = tasmax_c.sel(lat=latitude, lon=longitude, method='nearest')
+        airtemp = value.max(dim="time").item()
+
+        #if airtemp is still fucked generate random value lol
+        if (math.isnan(airtemp)):
+            airtemp = random.randint(0,40)
         flatsim = simheat(flat,airtemp)
         
         # Calculate energy usage heatmap and statistics
